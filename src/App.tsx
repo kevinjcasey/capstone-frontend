a0010e86de31af65660c7f5be072534b707b6f4c
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-27 16:57:41 -0400

    got something at least

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Questions from './components/Questions';
 
-type Props = {
-  question: string;
-}
+// type Props = {
+//   question: string;
+//   answer: string;
+//   categorgy: string;
+// }
 
-const App: React.FC<Props> = () => {
+const App = () => {
 
-  const [question, setQuestion] = useState()
+  const [question, setQuestion] = useState([])
+  // const [category, setCategory] = useState('')
+  // const [answer, setAnswer] = useState('')
 
-  const getTrivia = () => {
-    axios.get('https://opentdb.com/api.php?amount=10&category=11&type=multiple').then((res) => {
-      setQuestion(res.data.question)
-      console.log(question);
-      
-    })
-  }
+  const getTrivia = () => axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
+    setQuestion(res.data.results[0].question);
+    // setAnswer(res.data)
+    // setCategory(res.data.categorgy);
+    console.log(question);
+    // console.log(answer);
+    // console.log(category);
+
+
+
+  })
 
   useEffect(() => {
     getTrivia()
