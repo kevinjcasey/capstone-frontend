a07bf243699541b207b192c8d9166baac560ce88
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-03-28 13:13:22 -0400

    .replaceAll added for unicode

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Questions from './components/Questions';
 
-// type Props = {
-//   question: string;
-//   answer: string;
-//   categorgy: string;
-// }
+type Props = {
+  question: string;
+  answer: string;
+  categorgy: string;
+}
 
 const App = () => {
 
@@ -15,16 +15,18 @@ const App = () => {
   // const [answer, setAnswer] = useState('')
 
   const getTrivia = () => axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
-    setQuestion(res.data.results[0].question);
+    setQuestion(res.data.results[0].question.replaceAll('&quot;', '"').replaceAll('&#039:', '`'));
     // setAnswer(res.data)
     // setCategory(res.data.categorgy);
-    console.log(question);
+    console.log(res.data.results[0].question);
     // console.log(answer);
     // console.log(category);
-
+    // decodeURIComponent(res.data.results[0].question)
 
 
   })
+
+
 
   useEffect(() => {
     getTrivia()
@@ -34,9 +36,8 @@ const App = () => {
     <div className="App">
       <h1>Movie Trivia</h1>
       <h3>{question}</h3>
-      {/* <Questions 
-        question={question}
-      /> */}
+      <Questions question={''} answers={[]} userAnswer={undefined} questionNumber={0} totalQuestions={0}        
+      />
     </div>
   );
 }
